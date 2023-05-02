fd12c22f608f45a9cb542105004f2d207d764a1d
Carlos <carlos.moreno27@alu.uclm.es>
2023-05-02 17:38:34 +0200

    Beerute_Login03 update and Beerute_Login04 creation
    Actualización de Beerute_Login03 y creación de Beerute_Login04 (posterior implementación de login por botón de cuenta de Google)

---
+++ Beerute_Login04/app/src/main/java/com/example/beerute_login04/AuthActivity.kt
@@ -0,0 +1,96 @@
+package com.example.beerute_login04
+
+import android.app.AlertDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+
+class AuthActivity : AppCompatActivity() {
+
+    lateinit var emailEditText: EditText
+    lateinit var passwordEditText: EditText
+    lateinit var signUpButton: Button
+    lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        /*// Splash
+        * Thread.sleep(millis: 2000) //HACK
+        * setTheme(R.layout.activity_auth)*/
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        // Analytics Event
+        val analytics : FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
+
+        // Setup
+        setup()
+    }
+
+    private fun setup() {
+
+        title = "Autenticación"
+
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        signUpButton = findViewById(R.id.signUpButton)
+        loginButton = findViewById(R.id.loginButton)
+
+        signUpButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(emailEditText.text.toString(),
+                        passwordEditText.text.toString()).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
+        }
+
+        loginButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(emailEditText.text.toString(),
+                        passwordEditText.text.toString()).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun showAlert() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+}
---
+++ Beerute_Login04/app/src/main/java/com/example/beerute_login04/HomeActivity.kt
@@ -0,0 +1,47 @@
+package com.example.beerute_login04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
+enum class ProviderType {
+    BASIC
+}
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var emailTextView: TextView
+    lateinit var providerTextView: TextView
+    lateinit var logOutButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        // Setup
+
+        val bundle:Bundle? = intent.extras
+        val email:String? = bundle?.getString("email")
+        val provider:String? = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+    }
+
+    private fun setup(email: String, provider: String) {
+
+        title = "Inicio"
+
+        emailTextView = findViewById(R.id.emailTextView)
+        providerTextView = findViewById(R.id.providerTextView)
+        logOutButton = findViewById(R.id.logOutButton)
+
+        emailTextView.text = email
+        providerTextView.text = provider
+
+        logOutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+    }
+}
